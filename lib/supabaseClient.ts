0e7f4277d7b3d1b0aff8f46d9682f70364f19500
The REAL Blast-Pop <40577633+Blast-Pop@users.noreply.github.com>
2025-06-26 16:25:21 -0400

    Update supabaseClient.ts

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,7 +1,7 @@
 // supabaseClient.ts
 import { createClient } from '@supabase/supabase-js';
 
-const SUPABASE_URL = 'https://rihjknasguirrqwjporm.supabase.co'; // remplace par ton URL
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJpaGprbmFzZ3VpcnJxd2pwb3JtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA5NjA1NjcsImV4cCI6MjA2NjUzNjU2N30.swk6vo2GCU1N1ZhRSEKrZZ9dnYUJU-B88PLMOPStPbY'; // remplace par ta clé anonyme
+const SUPABASE_URL = 'SUPABASE URL'; // remplace par ton URL
+const SUPABASE_ANON_KEY = 'ANON KEY'; // remplace par ta clÃ© anonyme
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
